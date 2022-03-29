2d54d90f7bdf305e706f68203dfaa999423dba02
Cogno-Marco <marco.cognolato.98@gmail.com>
2022-03-29 13:47:35 +0200

    Multiple nodes states
    + implemented node link function
    ~ improved methods to work with strings instead of created objects, to improve api usage
    - remove useless states_created variable

--- PyMermaid/state_diagram/_internal_state_diagram.py
+++ PyMermaid/state_diagram/_internal_state_diagram.py
@@ -1,6 +1,4 @@
-from typing import List
-
-states_created: List["State"] = []
+from typing import List, Union
 
 code: List[str] = []
 
@@ -12,11 +10,32 @@ class State:
     def get_name(self):
         return self.name
 
-def link_from_start(state: State):
-    code.append(f"[*] --> {state.get_name()}")
+def link_from_start(state: Union[State, str]):
+    if isinstance(state, State):
+        code.append(f"[*] --> {state.get_name()}")
+    elif isinstance(state, str):
+        code.append(f"[*] --> {state}")
+    else:
+        raise TypeError("Input type must be str or Union")
 
-def link_to_end(state: State):
-    code.append(f"{state.get_name()} --> [*]")
+def link_to_end(state: Union[State, str]):
+    if isinstance(state, State):
+        code.append(f"{state.get_name()} --> [*]")
+    elif isinstance(state, str):
+        code.append(f"{state} --> [*]")
+    else:
+        raise TypeError("Input type must be str or Union")
+
+def link(stateFrom: Union[State, str], stateTo: Union[State, str], text: str = None):
+    if not isinstance(stateFrom, State) and not isinstance(stateFrom, str):
+        raise TypeError("stateFrom type must be str or Union")
+    if not isinstance(stateTo, State) and not isinstance(stateFrom, str):
+        raise TypeError("stateTo type must be str or Union")
+    
+    extra = f": {text}" if text else ""
+    from_name = stateFrom.get_name() if isinstance(stateFrom, State) else stateFrom
+    to_name   = stateTo.get_name()   if isinstance(stateTo, State)   else stateTo
+    code.append(f"{from_name} --> {to_name}{extra}")
 
 def evaluate() -> str:
     output = "stateDiagram-v2\n"
@@ -30,7 +49,11 @@ def evaluate() -> str:
     return output
 
 if __name__ == "__main__":
-    node = State("Test")
-    link_from_start(node)
-    link_to_end(node)
+    n1 = State("Test")
+    n2 = State("Second")
+    link_from_start(n1)
+    link_from_start("Prova")
+    link("Prova", n2)
+    link(n1, n2, "This is a transition")
+    link_to_end(n2)
     print(evaluate())
