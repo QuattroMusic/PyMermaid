fee99bc9477e4dc73a1fc62dacce9f1148d23118
Cogno-Marco <marco.cognolato.98@gmail.com>
2022-03-29 13:56:28 +0200

    Node description
    + implemented node description into node states

--- PyMermaid/state_diagram/_internal_state_diagram.py
+++ PyMermaid/state_diagram/_internal_state_diagram.py
@@ -1,14 +1,20 @@
 from typing import List, Union
 
 code: List[str] = []
+nodes_created: List["State"] = []
 
 # state object used by state diagrams
 class State:
-    def __init__(self, name: str):
+    def __init__(self, name: str, description: str = None):
         self.name = name
+        self.description = description
+        nodes_created.append(self)
     
     def get_name(self):
         return self.name
+    
+    def get_description(self):
+        return self.description
 
 def link_from_start(state: Union[State, str]):
     if isinstance(state, State):
@@ -40,17 +46,24 @@ def link(stateFrom: Union[State, str], stateTo: Union[State, str], text: str = N
 def evaluate() -> str:
     output = "stateDiagram-v2\n"
     indent_level = 1
+    
+    # setup all nodes with descriptions
+    for node in nodes_created:
+        if node.description:
+            output += " " * (indent_level * 4) + f"state \"{node.get_description()}\" as {node.get_name()}\n"
+    
+    # create whole document
     for line in code:
-        if "state" in line: indent_level += 1
-        if "}" in line:     indent_level -= 1
+        if "{" in line: indent_level += 1
+        if "}" in line: indent_level -= 1
         
         output += " " * (indent_level * 4) + f"{line}\n"
     
     return output
 
 if __name__ == "__main__":
-    n1 = State("Test")
-    n2 = State("Second")
+    n1 = State("Test", "This is a test description")
+    n2 = State("Second", "Second description")
     link_from_start(n1)
     link_from_start("Prova")
     link("Prova", n2)
